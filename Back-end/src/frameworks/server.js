d4224a3a53a1e2ccb7f8ff86e6a628659fd1682c
Joao-Paulo-Dev-Initial <joaop.dev.oliveira@gmail.com>
2026-06-08 09:40:37 -0300

    Ajustes para deploy do backend

--- Back-end/src/frameworks/server.js
+++ Back-end/src/frameworks/server.js
@@ -6,11 +6,11 @@ const cors = require("cors");
 const path = require("path");
 
 const server = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 server.use(cors());
 server.use(express.json());
 server.use("/uploads", express.static(path.join(__dirname, "uploads")));
 server.use(routes);
 
-server.listen(port, () => console.log("Server ON"));
+server.listen(port, () => console.log(`Server ON na porta ${port}`));
